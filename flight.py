57366dc0bdf831c466e4e19e897fa4069b178325
Ronaldo Rugieri <71158407+rugieri@users.noreply.github.com>
2020-11-07 13:52:09 -0300

    Add files via upload
    A simple example of a flight booking system using POO

---
+++ flight.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  test.py
+
+			
+class Flight():
+	def __init__(self, capacity):
+		self.capacity = capacity
+		self. passengers = []
+		
+	def add_passenger(self, name):
+		if not self.open_seats():
+			return False
+		self.passengers.append(name)
+		return True
+	
+	def open_seats(self):
+		return self.capacity - (len(self.passengers))		
+
+flight = Flight(3) 	
+people = ["Harry", "Ron", "Hermione", "Ginny"]	
+for person in people:
+	if flight.add_passenger(person):
+		print(f"Added {person} to flight successfully.")
+	else:
+		print(f"No available seats for {person}")	
